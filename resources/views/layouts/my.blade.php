101ca04d42bcb52cc2a8c3762b251fe3e0790991
horitomo <ths75149@gmail.com>
2019-05-30 16:41:51 +0900

    [ADD] 自作ヘルパ関数作成

--- resources/views/layouts/my.blade.php
+++ resources/views/layouts/my.blade.php
@@ -34,11 +34,21 @@
 
                                                                                                                         <ul class="navbar-nav mr-auto">
                                                                                                                         {{-- 「記事」と「ユーザー」へのリンク --}}
-                                                                                                                                            <li class="nav-item">
-                                                                                                                                                                <a class="nav-link" href="{{ url('posts') }}">{{ __('Posts') }}</a>
+                                                                                                                                            <li class="nav-item @if (my_is_current_controller('posts')) active @endif">
+                                                                                                                                                       <a class="nav-link" href="{{ url('posts') }}">
+                                                                                                                                                                {{ __('Posts') }}
+                                                                                                                                                                @if (my_is_current_controller('posts'))
+                                                                                                                                                                <span class="sr-only">(current)</span>
+                                                                                                                                                                @endif
+                                                                                                                                                                </a>
                                                                                                                                             </li>
-                                                                                                                                            <li class="nav-item">
-                                                                                                                                            <a class="nav-link" href="{{ url('users') }}">{{ __('Users') }}</a>
+                                                                                                                                            <li class="nav-item @if (my_is_current_controller('users')) active @endif">
+                                                                                                                                                                <a class="nav-link" href="{{ url('users') }}">
+                                                                                                                                                                                    {{ __('Users') }}
+                                                                                                                                                                                    @if (my_is_current_controller('users'))
+                                                                                                                                                                                    <span class="sr-only">(current)</span>
+                                                                                                                                                                                    @endif
+                                                                                                                                                                </a>
                                                                                                                                             </li>
                                                                                                                         </ul>
 
@@ -54,11 +64,21 @@
                                                                                                                                             {{-- 認証関連のリンク --}}
                                                                                                                                             @guest
                                                                                                                                             {{-- [ログイン]と[ユーザー登録]へのリンク --}}
-                                                                                                                                            <li class="nav-item">
-                                                                                                                                                                <a href=" {{route('login')}} " class="nav-link">{{__('Login')}}</a>
+                                                                                                                                            <li class="nav-item @if (my_is_current_controller('login', 'password')) active @endif">
+                                                                                                                                                                <a class="nav-link" href="{{ route('login') }}">
+                                                                                                                                                                {{ __('Login') }}
+                                                                                                                                                                @if (my_is_current_controller('login', 'password'))
+                                                                                                                                                                                    <span class="sr-only">(current)</span>
+                                                                                                                                                                @endif
+                                                                                                                                                                </a>
                                                                                                                                             </li>
-                                                                                                                                            <li class="nav-item">
-                                                                                                                                                                <a href="{{ route('register') }}" class="nav-link">{{__('Register')}}</a>
+                                                                                                                                            <li class="nav-item @if (my_is_current_controller('register')) active @endif">
+                                                                                                                                                                <a class="nav-link" href="{{ route('register') }}">
+                                                                                                                                                                {{ __('Register') }}
+                                                                                                                                                                @if (my_is_current_controller('register'))
+                                                                                                                                                                                    <span class="sr-only">(current)</span>
+                                                                                                                                                                @endif
+                                                                                                                                                                </a>
                                                                                                                                             </li>
 
                                                                                                                                             @else
