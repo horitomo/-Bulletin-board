81428c1f6cbb04383b78274c4918576830466ef2
horitomo <ths75149@gmail.com>
2019-06-10 11:48:55 +0900

    [UPD] errorページ表示変更

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,11 @@
-@extends('errors::minimal')
-
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@php
+    $title = __('Not Found');
+@endphp
+@extends('layouts.my')
+@section('content')
+<div class="container">
+    <h1>{{ $title }}</h1>
+    <p><strong>{{ __('Error') }}: <span class="error-code">404</span></strong></p>
+    <p>{{ __('The requested page does not exist.') }}</p>
+</div>
+@endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,11 @@
-@extends('errors::minimal')
-
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+@php
+    $title = __('Internal Server Error');
+@endphp
+@extends('layouts.my')
+@section('content')
+<div class="container">
+    <h1>{{ $title }}</h1>
+    <p><strong>{{ __('Error') }}: <span class="error-code">500</span></strong></p>
+    <p>{{ __('The server was unable to complete your request.') }}</p>
+</div>
+@endsection
